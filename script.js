9c2fd368506757fa37de458bd27b877da7ccb37d
unknown <embun.2021th@gmail.com>
2026-09-03 00:26:11 -0700

    praktikum XI RPL 1

---
+++ script.js
@@ -0,0 +1,15 @@
+function tampilkanNama(){
+    document.getElementById("namaAnggota").innerHTML=`
+
+    <ol
+        style="list-style-type: decimal;
+        padding-left:5%">
+        <li>Rindu (Rindu@gmail.com)</li>
+        <li>Embun (Embun@gmail.com)</li>
+    </ol>
+
+     <button onclick="location.reload()">
+        Tutup Kembali
+     </button> 
+     `;
+}
